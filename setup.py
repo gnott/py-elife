62b64c89cfbf34ca56bd1dd8fcfa11e6776f5228
Graham Nott <gnott@starglobal.ca>
2015-02-03 16:56:13 -0800

    Description and readme.

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from distutils.core import setup
 
 import pyelife
 
+with open('README.md') as fp:
+    readme = fp.read()
+
 with open('LICENSE') as fp:
     license = fp.read()
 
 setup(name='pyelife',
       version=pyelife.__version__,
+      description='Simple python distribution - testing.',
+      long_description=readme,
       packages=['pyelife'],
       license = license
       )
