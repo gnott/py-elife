2ed230384577ad10f8a6ba3609930ff4b9ac5f48
Graham Nott <gnott@starglobal.ca>
2015-02-03 16:48:55 -0800

    Add version for testing.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
+import pyelife
+
 with open('LICENSE') as fp:
     license = fp.read()
 
 setup(name='pyelife',
-      version='0.0.1',
+      version=pyelife.__version__,
       packages=['pyelife'],
       license = license
       )
